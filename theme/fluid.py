94dfc4fd92ce908b8547888d42dd5f544a58d1dd
brqs <3447851674@qq.com>
2021-06-18 16:44:22 +0800

    rssbeta

--- theme/fluid.py
+++ theme/fluid.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 import datetime
 from request_data import request
 
-# matery 友链规则
-def matery_get_friendlink(friendpage_link, friend_poor):
+# fluid 友链规则
+def get_friendlink(friendpage_link, friend_poor):
     result = request.get_data(friendpage_link)
     soup = BeautifulSoup(result, 'html.parser')
-    main_content = soup.find_all('div', {"class": "friend-div"})
+    main_content = soup.find_all('div', {"class": "card-content"})
     for item in main_content:
         img = item.find('img').get('src')
-        link = item.find('a').get('href')
-        name = item.find('h1').text
+        link = item.find('div', {"class": "link-intro"}).text
+        name = item.find('div', {"class": "link-title"}).text
         if "#" in link:
             pass
         else:
@@ -29,48 +29,49 @@ def matery_get_friendlink(friendpage_link, friend_poor):
             print('主页链接%r' % link)
             friend_poor.append(user_info)
 
-# 从matery主页获取文章
-def get_last_post_from_matery(user_info,post_poor):
-            error_sitmap = 'false'
+# 从fluid主页获取文章
+def get_last_post(user_info,post_poor):
+            error_sitmap = False
             link = user_info[1]
             print('\n')
-            print('-------执行matery主页规则----------')
+            print('-------执行fluid主页规则----------')
             print('执行链接：', link)
             result = request.get_data(link)
             soup = BeautifulSoup(result, 'html.parser')
-            main_content = soup.find_all(id='articles')
-            time_excit = soup.find_all('span',{"class": "publish-date"})
+            main_content = soup.find_all(id = 'board')
+            time_excit = soup.find_all('div',{"class": "post-meta mr-3"})
             if main_content and time_excit:
-                error_sitmap = 'true'
-                link_list = main_content[0].find_all('span', {"class": "publish-date"})
+                error_sitmap = True
+                link_list = main_content[0].find_all('div', {"class": "post-meta mr-3"})
                 lasttime = datetime.datetime.strptime('1970-01-01', "%Y-%m-%d")
                 for index, item in enumerate(link_list):
                     time = item.text
                     time = time.replace("|","")
                     time = time.replace(" ", "")
                     time = time.replace("\n", "")
+                    try: datetime.datetime.strptime(time, "%Y-%m-%d")
+                    except: continue
                     if lasttime < datetime.datetime.strptime(time, "%Y-%m-%d"):
                         lasttime = datetime.datetime.strptime(time, "%Y-%m-%d")
                 lasttime = lasttime.strftime('%Y-%m-%d')
                 print('最新时间是', lasttime)
-                last_post_list = main_content[0].find_all('div', {"class": "card"})
+                last_post_list = main_content[0].find_all('div', {"class": "row mx-auto index-card"})
+
                 for item in last_post_list:
-                    time_created = item.find('span', {"class": "publish-date"}).text.strip()
+                    time_created = item.find('div', {"class": "post-meta mr-3"}).text.strip()
+
                     if time_created == lasttime:
-                        error_sitmap = 'false'
-                        print(lasttime)
+                        error_sitmap = False
                         a = item.find('a')
                         # print(item.find('a'))
-                        alink = a['href']
-                        alinksplit = alink.split("/", 1)
-                        stralink = alinksplit[1].strip()
+                        stralink = a['href']
                         if link[-1] != '/':
                             link = link + '/'
-                        print(item.find('span', {"class": "card-title"}).text.strip().encode("gbk", 'ignore').decode('gbk', 'ignore'))
+                        print(item.find('h1', {"class": "index-header"}).text.strip().encode("gbk", 'ignore').decode('gbk', 'ignore'))
                         print(link + stralink)
                         print("-----------获取到匹配结果----------")
                         post_info = {
-                            'title': item.find('span', {"class": "card-title"}).text.strip(),
+                            'title': item.find('h1', {"class": "index-header"}).text.strip(),
                             'time': lasttime,
                             'link': link + stralink,
                             'name': user_info[0],
@@ -78,8 +79,8 @@ def get_last_post_from_matery(user_info,post_poor):
                         }
                         post_poor.append(post_info)
             else:
-                error_sitmap = 'true'
-                print('貌似不是类似matery主题！')
-            print("-----------结束matery主页规则----------")
+                error_sitmap = True
+                print('貌似不是类似fluid主题！')
+            print("-----------结束fluid主页规则----------")
             print('\n')
             return error_sitmap
--- theme/matery.py
+++ theme/matery.py
@@ -4,32 +4,15 @@ from bs4 import BeautifulSoup
 import datetime
 from request_data import request
 
-# butterfly 友链规则
-def butterfly_get_friendlink(friendpage_link, friend_poor):
+# matery 友链规则
+def get_friendlink(friendpage_link, friend_poor):
     result = request.get_data(friendpage_link)
     soup = BeautifulSoup(result, 'html.parser')
-    main_content = soup.find_all(id='article-container')
-    link_list = main_content[0].find_all('a')
-    for index, item in enumerate(link_list):
-        link = item.get('href')
-        if link.count('/') > 3:
-            continue
-        if item.get('title'):
-            name = item.get('title')
-        else:
-            try:
-                name = item.find('span').text
-            except:
-                continue
-        try:
-            if len(item.find_all('img')) > 1:
-                imglist = item.find_all('img')
-                img = imglist[1].get('data-lazy-src')
-            else:
-                imglist = item.find_all('img')
-                img = imglist[0].get('data-lazy-src')
-        except:
-            continue
+    main_content = soup.find_all('div', {"class": "friend-div"})
+    for item in main_content:
+        img = item.find('img').get('src')
+        link = item.find('a').get('href')
+        name = item.find('h1').text
         if "#" in link:
             pass
         else:
@@ -46,43 +29,35 @@ def butterfly_get_friendlink(friendpage_link, friend_poor):
             print('主页链接%r' % link)
             friend_poor.append(user_info)
 
-# 从butterfly主页获取文章
-def get_last_post_from_butterfly(user_info,post_poor):
-            error_sitmap = 'false'
+# 从matery主页获取文章
+def get_last_post(user_info,post_poor):
+            error_sitmap = False
             link = user_info[1]
             print('\n')
-            print('-------执行butterfly主页规则----------')
+            print('-------执行matery主页规则----------')
             print('执行链接：', link)
             result = request.get_data(link)
             soup = BeautifulSoup(result, 'html.parser')
-            main_content = soup.find_all(id='recent-posts')
-            time_excit = soup.find_all('time')
+            main_content = soup.find_all(id='articles')
+            time_excit = soup.find_all('span',{"class": "publish-date"})
             if main_content and time_excit:
-                error_sitmap = 'true'
-                link_list = main_content[0].find_all('time', {"class": "post-meta-date-created"})
-                if link_list == []:
-                    print('该页面无文章生成日期')
-                    link_list = main_content[0].find_all('time')
-                else:
-                    print('该页面有文章生成日期')
+                error_sitmap = True
+                link_list = main_content[0].find_all('span', {"class": "publish-date"})
                 lasttime = datetime.datetime.strptime('1970-01-01', "%Y-%m-%d")
                 for index, item in enumerate(link_list):
                     time = item.text
                     time = time.replace("|","")
                     time = time.replace(" ", "")
+                    time = time.replace("\n", "")
                     if lasttime < datetime.datetime.strptime(time, "%Y-%m-%d"):
                         lasttime = datetime.datetime.strptime(time, "%Y-%m-%d")
                 lasttime = lasttime.strftime('%Y-%m-%d')
                 print('最新时间是', lasttime)
-                last_post_list = main_content[0].find_all('div', {"class": "recent-post-info"})
+                last_post_list = main_content[0].find_all('div', {"class": "card"})
                 for item in last_post_list:
-                    time_created = item.find('time', {"class": "post-meta-date-created"})
-                    if time_created:
-                        pass
-                    else:
-                        time_created = item
-                    if time_created.find(text=lasttime):
-                        error_sitmap = 'false'
+                    time_created = item.find('span', {"class": "publish-date"}).text.strip()
+                    if time_created == lasttime:
+                        error_sitmap = False
                         print(lasttime)
                         a = item.find('a')
                         # print(item.find('a'))
@@ -91,11 +66,11 @@ def get_last_post_from_butterfly(user_info,post_poor):
                         stralink = alinksplit[1].strip()
                         if link[-1] != '/':
                             link = link + '/'
-                        print(a.text.encode("gbk", 'ignore').decode('gbk', 'ignore'))
+                        print(item.find('span', {"class": "card-title"}).text.strip().encode("gbk", 'ignore').decode('gbk', 'ignore'))
                         print(link + stralink)
                         print("-----------获取到匹配结果----------")
                         post_info = {
-                            'title': a.text,
+                            'title': item.find('span', {"class": "card-title"}).text.strip(),
                             'time': lasttime,
                             'link': link + stralink,
                             'name': user_info[0],
@@ -103,8 +78,8 @@ def get_last_post_from_butterfly(user_info,post_poor):
                         }
                         post_poor.append(post_info)
             else:
-                error_sitmap = 'true'
-                print('貌似不是类似butterfly主题！')
-            print("-----------结束butterfly主页规则----------")
+                error_sitmap = True
+                print('貌似不是类似matery主题！')
+            print("-----------结束matery主页规则----------")
             print('\n')
             return error_sitmap
